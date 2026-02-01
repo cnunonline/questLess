02faf547a36bf88f1f5277edb9b5963b1aa783ce
肖鑫 <2543920310@qq.com>
2026-02-01 16:25:17 +0800

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module xx_personal.questless {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens xx_personal.questless to javafx.fxml;
+    exports xx_personal.questless;
+}
